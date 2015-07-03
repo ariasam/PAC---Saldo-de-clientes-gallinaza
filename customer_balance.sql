6eaeced3208f23ccf8377d8de0e58bf0896efe59
ariasam <ariasam@gmail.com>
2015-07-03 16:36:16 -0500

    Agregué condiciones where
    Añadí header_id y line_id

--- customer_balance.sql
+++ customer_balance.sql
@@ -1,1 +1,3 @@
-select * from ont.oe_order_lines_all oola, ont.or_order_headers_all ooha;
+select * from ont.oe_order_lines_all oola, ont.oe_order_headers_all ooha
+where ooha.header_id= 123234
+and oola.line_id=11234;
